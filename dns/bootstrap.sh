2f80d3a4d8103945e4bd37d8dcea69652349dd65
Serge Dukic <serge@vendhq.com>
2015-07-06 14:00:34 +1200

    Changed bootstrap added secondary server config
    Modified the bootstrap script to actually work properly and added all of
    the secondary server configuration into the repository.
    
    Also, note that the "allow transfer" stanza's should use the public IP's
    of the DNS servers.
    
    The secondary server has to be modified to make the zone file live in
    /var/cache/bind/[zone name], due to AppArmor:
    http://ubuntuforums.org/showthread.php?t=1425836

--- dns/bootstrap.sh
+++ dns/bootstrap.sh
@@ -4,6 +4,52 @@ echo "Updating..."
 apt-get update
 echo "Installing fortune..."
 apt-get -y install fortune
-echo "Outputting to file..."
+echo "Outputting fortune to file..."
 /usr/games/fortune > /root/hello_world.txt
 echo "Done."
+
+echo "Installing BIND..."
+apt-get -y install bind9
+echo "Done."
+
+echo "Setting up the configuration..."
+# Create the public zone file
+cat <<EOF > /etc/bind/named.conf.public-zones
+zone "dukic.co.nz" {
+	type master;
+	file "/etc/bind/db.dukic.co.nz";
+};
+
+//zone "127.in-addr.arpa" {
+//	type master;
+//	file "/etc/bind/db.127";
+//};
+EOF
+
+#TODO: rewrite script to put IP's of Instances instead of the hard coded values
+#TODO: also, need to make sure it's the public and not private IP
+# Create the dukic.co.nz DNS db file
+cat <<EOF > /etc/bind/db.dukic.co.nz
+;
+; BIND data file for local loopback interface
+;
+\$TTL	604800
+@	IN	SOA	ns1.dukic.co.nz. admin.dukic.co.nz. (
+			      2		; Serial
+			 604800		; Refresh
+			  86400		; Retry
+			2419200		; Expire
+			 604800 )	; Negative Cache TTL
+;
+@	IN	NS	ns1.dukic.co.nz.
+@	IN	A	10.159.95.235
+@	IN	AAAA	::1
+ns1	IN	A	10.159.95.235
+EOF
+
+# Include the public zone file
+echo "include \"/etc/bind/named.conf.public-zones\";" >> /etc/bind/named.conf
+
+echo "Restarting BIND..."
+service bind9 restart
+echo "All done... please test :-)"
